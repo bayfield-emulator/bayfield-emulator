8b8da35973488c4d1f13d6b245044dac7bce3deb
Nick <fuelsoft@users.noreply.github.com>
2020-11-24 01:08:47 -0800

    File picker type filter

--- src/file_picker_windows.cpp
+++ src/file_picker_windows.cpp
@@ -4,6 +4,10 @@
 
 #include "file_picker.h"
 
+/*
+    This uses the old, XP-style GetOpenFileName picker. 
+    In a perfect world, it would be migrated to Common Item Dialog type.
+*/
 int fp_get_user_path(char **path_out) {
     OPENFILENAMEW params = {0};
     wchar_t *wide_filename = new wchar_t[MAX_PATH];
@@ -12,6 +16,7 @@ int fp_get_user_path(char **path_out) {
     params.lStructSize = sizeof(params);
     params.lpstrFile = wide_filename;
     params.nMaxFile = MAX_PATH;
+    params.lpstrFilter = L"Gameboy ROM Files\0*.GB;*.ROM\0All Files\0*.*\0\0";
     params.lpstrTitle = L"Select ROM";
     params.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
     BOOL ok = GetOpenFileNameW(&params);
